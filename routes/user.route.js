296fec713e303ddf302cefa272c9e3eb477db4dc
suhel <thecomputersk@gamil.com>
2025-02-10 22:21:15 +0530

    confirm

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,7 @@ const authMiddleware = require('../middlewares/auth.middleware');
 router.post('/signup', userController.signup);
 router.post('/signin', userController.signin);
 router.post('/logout', userController.logout);
+
 router.get('/profile', authMiddleware.isAuthenticated, userController.getProfile);
 router.get('/products', authMiddleware.isAuthenticated, userController.getProducts);
 router.get('/products/:id', authMiddleware.isAuthenticated, userController.getProductById);
